fa330785ea1026412b593dc8b401b2cc69865842
carlohartelucci <charliehartel@yahoo.com>
2012-09-05 15:54:49 -0400

    change active day

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -4,7 +4,13 @@ class ScheduleController < ApplicationController
 
 	def current_week
 		@current_week = current_user.current_week
-		@today = current_user.today
+		@base_path = request.fullpath.split('?')[0]
+
+		if params['active'].nil?
+			@active_day = current_user.today
+		else
+			@active_day = Workout.find(params['active'])
+		end
 	end
 
 	protected
